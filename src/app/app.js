d08cf3de18973968a698400e2ff022234394b125
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-05-27 23:44:15 +0530

    App: Adds babel polyfill for async await

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,8 @@
+import 'babel-polyfill';
+
 import Runner from '../game/js/';
+import MultilayerPerceptronBuilder from '../models/mlp/mlp';
 
 const play = new Runner('.interstitial-wrapper');
+
+const mlp = new MultilayerPerceptronBuilder();
